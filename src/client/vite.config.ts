0e42d12ce71dc32e725459527356508b77773f19
Vishal Thapa <46962439+kratos0002@users.noreply.github.com>
2024-12-20 15:46:28 +0000

    added vercel json

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,8 +1,13 @@
+// src/client/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    outDir: './dist',
+  },
   server: {
     proxy: {
       '/api': {
